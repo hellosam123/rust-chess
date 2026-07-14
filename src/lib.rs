e0c3329e2058284d5274db35ce5b1bf8bf23ecdd
samuel <typewhatrecord1@gmail.com>
2026-07-14 10:55:35 +0800

    Move module declarations to lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+mod attacks;
+mod bitboard;
+pub mod board;
+mod evaluate;
+mod magic_bitboard;
+pub mod movegen;
+mod mv;
+mod search;
+mod zobrist;
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,3 @@
-mod attacks;
-mod bitboard;
-mod board;
-mod evaluate;
-mod magic_bitboard;
-mod movegen;
-mod mv;
-mod search;
-mod zobrist;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let b = board::Board::new_starting_board()?;
-    b.print_board();
-
     Ok(())
 }
